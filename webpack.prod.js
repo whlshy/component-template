69a84754544d14343e02ea9c93a64e3da554fd7c
盧韋宏 <whl@wke.csie.ncnu.edu.tw>
2023-01-16 10:22:31 +0800

    build 壓縮js並取消console.log

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,8 @@
 const { merge } = require('webpack-merge'),
   common = require('./webpack.common.js'),
   webpack = require('webpack')
-//TerserPlugin = require('terser-webpack-plugin')
+TerserPlugin = require('terser-webpack-plugin')
+// const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = merge(common, {
   mode: 'production',
@@ -17,5 +18,27 @@ module.exports = merge(common, {
       'react-dom': 'react-dom',
       'prop-types': 'prop-types',
     },
-  ]
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      // 壓縮JS
+      new TerserPlugin({
+        test: /\.js(x)?(\?.*)?$/i,
+        exclude: /node_modules/,
+        terserOptions: {
+          compress: {
+            warnings: false, // 當刪除沒有用處的代碼時，顯示警告
+            drop_console: true // 刪除console.*函數
+          },
+          output: {
+            beautify: false, // 是否美化輸出代碼
+            comments: false // 保留所有註釋
+          }
+        }
+      }),
+      // 壓縮CSS
+      // new OptimizeCSSAssetsPlugin()
+    ]
+  },
 })
